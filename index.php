93df3aee78ec8cb1d9f74d6256963f4af7fe283a
Marcos Urios <marcosurios@MacBook-Pro-de-Marcos.local>
2017-10-30 22:49:34 +0100

    ""Base de datos"" temporal creada

--- index.php
+++ index.php
@@ -5,106 +5,87 @@
 	require_once('includes/controlAcceso.php');
 ?>
 <!-- FIN CABECERA  ?> -->   
-    			
+	<?php 
+	/* ---- Base de datos temporal ---- */ 
+	$fotos = array
+	(
+	array(1,"Buenas&nbspvistas","29/09/2017","Afganistan", "Pepe"),
+	array(2,"Kite&nbsp;for&nbsp;life","17/06/2017","España", "Alberto"),
+	array(3,"BOOM","08/04/2017","Perú","Paco"),
+	array(4,"Good&nbsp;views","12/02/2017","Rio%20Janeiro","Alberto"),
+	array(5,"Life&nbsp;style","03/01/2017","Bali","Andres"),
+	);
+	?>		
 	<main>
 	<!-- GALERIA DE IMAGENES -->
 		<ul class="galeria">
 			<li class="foto">
-				<?php 
-				$titulo1="Buenas&nbsp;vistas";
-				$fecha1="29/09/2017";
-				$pais1="Afganistan";
-				$usuario1="Pepe";
 				
-				echo '<h2>' . $titulo1 . '</h2>
-				<a href="infIMG.php?fecha='.$fecha1.'&pais='.$pais1.'&titulo='.$titulo1.'&usuario='.$usuario1.'">
+				<h2> <?php echo $fotos[0][1] ?> </h2>
+				<a href="infIMG.php?id=1">
 					<img src="images/mountain.jpg" alt="Imagen">
 				</a>
 				<p>
-					'. $fecha1 .'
+					<?php echo $fotos[0][2] ?>
 				</p>
 				<p>
-					' . $pais1 . '
-				</p>'
-				?>
+					<?php echo $fotos[0][3] ?>
+				</p>
+				
 			</li>
 			<li class="foto">
-			<?php 
-				$titulo2="Kite&nbsp;for&nbsp;life";
-				$fecha2="17/06/2017";
-				$pais2="España";
-				$usuario2="Alberto";
-				
-				echo '<h2>' . $titulo2 . '</h2>
-				<a href="infIMG.php?fecha='.$fecha2.'&pais='.$pais2.'&titulo='.$titulo2.'&usuario='.$usuario2.'">
+				<h2> <?php echo $fotos[1][1] ?> </h2>
+				<a href="infIMG.php?id=2">
 					<img src="images/kite.jpg" alt="Imagen">
 				</a>
 				<p>
-					'. $fecha2 .'
+					<?php echo $fotos[1][2] ?>
 				</p>
 				<p>
-					' . $pais2 . '
-				</p>'
-				?>
+					<?php echo $fotos[1][3] ?>
+				</p>
+				
 			</li>
 			<li class="foto">
-			<?php 
-				$titulo3="BOOM";
-				$fecha3="08/04/2017";
-				$pais3="Perú";
-				$usuario3="Paco";
-				
-				echo '<h2>' . $titulo3 . '</h2>
-				<a href="infIMG.php?fecha='.$fecha3.'&pais='.$pais3.'&titulo='.$titulo3.'&usuario='.$usuario3.'">
+				<h2> <?php echo $fotos[2][1] ?> </h2>
+				<a href="infIMG.php?id=3">
 					<img src="images/volcan.jpg" alt="Imagen">
 				</a>
 				<p>
-					'. $fecha3 .'
+					<?php echo $fotos[2][2] ?>
 				</p>
 				<p>
-					' . $pais3 . '
-				</p>'
-				?>
+					<?php echo $fotos[2][3] ?>
+				</p>
+				
 			</li>
 			<li class="foto">
 
-			<?php 
-				$titulo4="Good&nbsp;views";
-				$fecha4="12/02/2017";
-				$pais4="Rio%20Janeiro";
-				$usuario4="Alberto";
-				
-				echo '<h2>' . $titulo4 . '</h2>
-				<a href="infIMG.php?fecha='.$fecha4.'&pais='.$pais4.'&titulo='.$titulo4.'&usuario='.$usuario4.'">
+			<h2> <?php echo $fotos[3][1] ?> </h2>
+				<a href="infIMG.php?id=4">
 					<img src="images/caribe.jpg" alt="Imagen">
 				</a>
 				<p>
-					'. $fecha4 .'
+					<?php echo $fotos[3][2] ?>
 				</p>
 				<p>
-					' . $pais4 . '
-				</p>'
-				?>
+					<?php echo $fotos[3][3] ?>
+				</p>
+				
 			</li>
 			<li class="foto">
 
-			<?php 
-				$titulo5="Life&nbsp;style";
-				$fecha5="03/01/2017";
-				$pais5="Bali";
-				$usuario5="Andres";
-				
-				echo '<h2>' . $titulo5 . '</h2>
-				<a href="infIMG.php?fecha='.$fecha5.'&pais='.$pais5.'&titulo='.$titulo5.'&usuario='.$usuario5.'">
+			<h2> <?php echo $fotos[4][1] ?> </h2>
+				<a href="infIMG.php?id=5">
 					<img src="images/surf.jpg" alt="Imagen">
 				</a>
 				<p>
-					'. $fecha5 .'
+					<?php echo $fotos[4][2] ?>
 				</p>
 				<p>
-					' . $pais5 . '
-				</p>'
-				?>
+					<?php echo $fotos[4][3] ?>
+				</p>
+				
 			</li>
 		</ul>
 		
--- infIMG.php
+++ infIMG.php
@@ -4,20 +4,30 @@
     require_once('includes/controlAcceso.php');
 ?>
 <!-- FIN CABECERA  ?> -->
-
+<?php 
+/* ---- Base de datos temporal ---- */ 
+$fotos = array
+(
+array(1,"Buenas&nbspvistas","29/09/2017","Afganistan", "Pepe", "Album de vacaciones"),
+array(2,"Kite&nbsp;for&nbsp;life","17/06/2017","España", "Alberto", "Album de deportes"),
+array(3,"BOOM","08/04/2017","Perú","Paco", "Album de Perú"),
+array(4,"Good&nbsp;views","12/02/2017","Rio%20Janeiro","Alberto", "Mi album"),
+array(5,"Life&nbsp;style","03/01/2017","Bali","Andres", "Album de mi vida"),
+);
+?>
     <main>   
         <div id="infofoto">
-         <h2>Titulo<?php echo $_GET["titulo"]?></h2>
+         <h2>Titulo: <?php echo $fotos[$_GET["id"]][1]?></h2>
            
                 <img src="images/mountain.jpg" alt="Imagen" style="margin-bottom: 10px;">
             <table>
                 <tr>
-                    <td>Fecha <?php echo $_GET["fecha"]?></td>
-                    <td>País <?php echo $_GET["pais"]?></td>
+                    <td>Fecha <?php echo $fotos[$_GET["id"]][2]?></td>
+                    <td>País <?php echo $fotos[$_GET["id"]][3]?></td>
                 </tr>
                 <tr>
-                    <td>Álbum <?php echo "desconocido"//$_GET["album"]?></td>
-                    <td>Usuario <?php echo $_GET["usuario"]?></td>
+                    <td>Álbum <?php echo $fotos[$_GET["id"]][5]?></td>
+                    <td>Usuario <?php echo $fotos[$_GET["id"]][4]?></td>
                 </tr>
             </table>
         </div>
