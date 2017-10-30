6ffd4cde0bba611f46245d7bdb269881ac41cc8e
Carlos Aldaravi Coll <carlosaldaravi@gmail.com>
2017-10-30 19:01:14 +0100

    quitadas las paginas que no servian para proteger

--- sesion/sesion.php
+++ sesion/sesion.php
@@ -1,11 +1,13 @@
 <?php  
     session_start();
     if(!isset($_SESSION['username'])){
-        require_once('includes/cabeceraEstilo2.inc');
+        require_once('includes/cabecera.inc');
         require_once('includes/header.php');
         ?>
+        <body>
         <p class="card">Debes iniciar sesión para acceder a este contenido</br></br>
         <a class"card" href="index.php">Volver a inicio</a></p>
+    
         <?php
         die();
     }
