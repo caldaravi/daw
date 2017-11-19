d9306492acbab916f4317eea7b88ec807bbc2712
Marcosaurios <marcosuriosgomez@gmail.com>
2017-11-20 00:57:11 +0100

    Header editado

--- includes/header.php
+++ includes/header.php
@@ -4,37 +4,29 @@
 ?>
 
 <body>
-<header class="b">
-	<ul class="no-style b">
+<header>
+	
+	<!-- / #logo-header -->
+	<a id="h_logo" href=<?php echo $urlLocal.'index.php'?>>
+		<img id="logo" src=<?php echo $urlLocal . "images/logo.png"?> alt="logo">
+	</a>
+	<div id="h_reg">
+		<a href=<?php echo $urlLocal.'registro/nuevoReg.php'?>>Regístrate</a>
+	</div>
+	<div id="h_busq">
+		<form action=<?php echo $urlLocal . "busqueda/formBusqueda.php"?> method="get">
+			<label>
+				<input type="text" placeholder="Busca fotos">
+			</label>
+			<input type="Submit" value="Buscar">
+		</form>
+	</div>
 
-		<li class="b" id="logoLi">
-			<!-- / #logo-header -->
-			<a href=<?php echo $urlLocal.'index.php'?>>
-				<img id="logo" src=<?php echo $urlLocal . "images/logo.png"?> alt="logo">
-			</a>
-		</li>
+	<?php include_once($urlLocal . "includes/acceso.php");?>
 
-		<li class="b" id="regLi">
-			<a id="botonRegId" href=<?php echo $urlLocal.'registro/nuevoReg.php'?>>Regístrate</a>
-		</li>
-
-		<li class="b" id="buscarLi">
-			<form action=<?php echo $urlLocal . "busqueda/formBusqueda.php"?> method="get">
-				<label>
-					<input type="text" placeholder="Busca fotos">
-				</label>
-				<input type="Submit" value="Buscar">
-			</form>
-		</li>
-
-		<li class="b" id="formregLi">
-			<?php include_once($urlLocal . "includes/acceso.php");?>
-		</li>
-		
-		<li id="loginicon">
-				<a>
-					<img id="loginimg" src="https://image.flaticon.com/icons/png/512/64/64572.png" alt="login icon">
-				</a>
-		</li>
-	</ul>
+	<!--	<a>
+			<img id="loginimg" src="https://image.flaticon.com/icons/png/512/64/64572.png" alt="login icon">
+		</a>
+	-->
+	
 </header>
